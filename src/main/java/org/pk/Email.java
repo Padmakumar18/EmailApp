b617a1712ad83b596ac6d5b14ea7f0abde417a9a
padmakumar18 <padmakumar41759@gmail.com>
2025-03-06 17:18:01 +0530

    1) Random pass method created 2) Need to finish other methods

--- src/main/java/org/pk/Email.java
+++ src/main/java/org/pk/Email.java
@@ -6,12 +6,18 @@ public class Email {
     private String firstName;
     private String lastName;
     private String password;
-    private String departmant;
+    private String department;
     private int mailboxCapacity;
+    private int defaultPassLen = 8;
 
     public Email(String firstName , String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+
+        this.department = setDepartment();
+        
+        this.password = randompassword(this.defaultPassLen);
+        System.out.println(this.password);
     }
 
     private String setDepartment() {
@@ -29,5 +35,15 @@ public class Email {
             default:
                 return  "";
         }
+    }
+
+    private String randompassword(int len) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()_+";
+        char[] pass = new char[len];
+        for (int i = 0; i < len; i++) {
+            int ran = (int)(Math.random()*passwordSet.length());
+            pass[i] = passwordSet.charAt(ran);
+         }
+         return new String(pass);
     }
 }
--- src/main/java/org/pk/Main.java
+++ src/main/java/org/pk/Main.java
@@ -2,6 +2,6 @@ package org.pk;
 
 public class Main {
     public static void main(String[] args) {
-
+        Email email = new Email("Padma", "Kumar");
     }
 }
